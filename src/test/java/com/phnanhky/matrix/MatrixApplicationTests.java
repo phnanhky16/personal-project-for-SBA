265fcfa70ceb17eb592eb298096b3892e1b775b4
phnanhky <kypase184884@fpt.edu.vn>
2025-10-07 14:07:15 +0700

    Add files via upload

---
+++ src/test/java/com/phnanhky/matrix/MatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phnanhky.matrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
